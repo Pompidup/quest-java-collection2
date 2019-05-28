26007d813e78f5030ec05d47e9fd3852768e3eaa
Pompidup <julienauvray28@gmail.com>
2019-05-28 17:33:13 +0200

    Add Comparable for solution of quest

--- Thanos.java
+++ Thanos.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Thanos {
 
     public static void main(String[] args) {
 
-        List<Hero> heroes = new ArrayList();
+        List<Hero> heroes = new ArrayList<>();
         heroes.add(new Hero("Hulk", 49));
         heroes.add(new Hero("Black Widow", 34));
         heroes.add(new Hero("Captain America", 100));
@@ -13,14 +15,23 @@ public class Thanos {
 
         // TODO 1 : Modify Hero to implements Comparable and sort by name (ascending)
 
+        Comparator<Hero> heroNameComparator = new Comparator<Hero>() {
+            @Override
+            public int compare(Hero hero1, Hero hero2) {
+                return hero1.getName().compareTo(hero2.getName());
+            }
+        };
 
         System.out.println("\nOrder by name:");
+        Collections.sort(heroes, heroNameComparator);
         showList(heroes);
+
 
         // TODO 2: Add a Comparator and sort by age (descending)
 
 
         System.out.println("\nOrder by age:");
+        Collections.sort(heroes);
         showList(heroes);
     }
 
@@ -29,4 +40,6 @@ public class Thanos {
             System.out.println(heroe.getName() + ", " + heroe.getAge());
         }
     }
+
+
 }
